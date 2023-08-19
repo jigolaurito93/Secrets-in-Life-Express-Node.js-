5bb3bf41febcc55845a639535a41bbbb1e2584e0
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-19 01:12:09 -0500

    import all necessary packages and middleware, use bodyParser middleware, initiate first route handler

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+// Import necessary packages and middleware
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Load index.html when get request is successful
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
